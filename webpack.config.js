a2002e1d8dc31503469076ba41259161f28c7217
percy <percy.zhang@logicmonitor.com>
2015-11-11 22:10:57 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path');
 
 module.exports = {
-    context: path.join(__dirname,  'lib'),
+    context: path.join(__dirname,  './lib'),
     entry: {
         Table: './Table.js'
     },
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, './dist'),
         filename: '[name].js',
         libraryTarget: 'umd',
         library: ['LMUI']
@@ -14,16 +14,29 @@ module.exports = {
     module: {
         loaders: [
             {
-                test: /.jsx?$/,
+                test: /\.jsx?$/,
                 exclude: /(node_modules|bower_components)/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'react'],
                     plugins: ['transform-es2015-modules-commonjs']
                 }
+            },
+            // SASS
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css?modules', 'sass', 'postcss']
+            },
+            // LESS
+            {
+                test: /\.less$/,
+                loaders: ['style', 'css?modules', 'less', 'postcss']
             }
         ]
     },
+    postcss: [
+        require('autoprefixer')
+    ],
     externals:[{
         'react': {
             root: 'React',
